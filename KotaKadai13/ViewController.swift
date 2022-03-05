af00e408643664bae4b2abd7b76f3da4106bb45e
MaedaKouta <maedakoutaa@gmail.com>
2022-03-05 13:13:11 +0900

    Initial Commit

---
+++ KotaKadai13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KotaKadai13
+//
+//  Created by 前田航汰 on 2022/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
